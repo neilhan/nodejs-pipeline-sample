5d32d8065ee9aedd15830c9b06c60efd28b34186
Neil Han <neil_han@intuit.com>
2018-11-29 11:08:03 -0800

    log api call event

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ exports.handler = (event, context, callback) => {
     callback(null, {
         statusCode: '200',
         body: 'The time in Los Angeles is: ' + currentTime.toString(),
+        release: '1',
     });
 };
